36167f7bb5c6da4240247533c8eafa34bcb5f2ed
Rey Dhuny <rey@me.com>
2014-03-30 17:42:18 +0100

    add su for all commands to be run by the user
    i think this can be done cleaner but this works

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,26 +25,21 @@ rvm use ruby-2.1.1 --default
 
 # get dotfiles
 echo "Get dotfiles"
-git clone https://github.com/rey/.dot.git /home/vagrant/.dot
+su -c "git clone https://github.com/rey/.dot.git /home/vagrant/.dot" - vagrant
 
 # install dotfiles
-echo "Install dotfiles"
 su -c "sh /home/vagrant/.dot/install.sh" - vagrant
 
 # get vim config
-# echo "Get vim config"
-# git clone https://github.com/rey/.vim.git /home/vagrant/.vim
+su -c "git clone https://github.com/rey/.vim.git /home/vagrant/.vim" - vagrant
 
 # install vim config
-# echo "Install vim config"
-# su -c "sh /home/vagrant/.vim/install.sh" - vagrant
+su -c "sh /home/vagrant/.vim/install.sh" - vagrant
 
 # get tmux config
-echo "Get tmux config"
-git clone https://github.com/rey/.tmux.git /home/vagrant/.tmux
+su -c "git clone https://github.com/rey/.tmux.git /home/vagrant/.tmux" - vagrant
 
 # install tmux config
-echo "Install tmux config"
 su -c "sh /home/vagrant/.tmux/install.sh" - vagrant
 
 # finish
